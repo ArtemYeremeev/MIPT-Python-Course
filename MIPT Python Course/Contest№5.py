8533c854a124e53283b6c10f690e6320145f79ea
ArtemYeremeev <nice.art228@mail.ru>
2018-10-21 18:22:15 +0300

    Update MIPT Python Course/Contest№5.py

---
+++ MIPT Python Course/Contest№5.py
@@ -0,0 +1,71 @@
+"""MIPT Python Course. Contest5"""
+from lib import graphics as gr
+
+print('1. Движение шара')
+SIZE_X = 400
+SIZE_Y = 400
+# Исходные данные
+window = gr.GraphWin("Model", SIZE_X, SIZE_Y)
+coords = gr.Point(200, 200)
+velocity = gr.Point(2, 0)
+
+def add(point_1, point_2):
+    """Функция, описывающая изменение положения шара"""
+    new_point = gr.Point(point_1.x + point_2.x,
+                         point_1.y + point_2.y)
+    return new_point
+
+def clear_window():
+    """Функция, очищающая поле"""
+    rectangle = gr.Rectangle(gr.Point(0, 0), gr.Point(SIZE_X, SIZE_Y))
+    rectangle.setFill('green')
+    rectangle.draw(window)
+
+def draw_ball(coords):
+    """Функция визуализации"""
+    circle = gr.Circle(coords, 15)
+    circle.setFill('yellow')
+    circle.draw(window)
+
+for t in range(100):
+    clear_window()
+    draw_ball(coords)
+    coords = add(coords, velocity)
+    gr.time.sleep(0.02)
+    t -= 1
+
+window.getMouse()
+window.close()
+
+print('2. Скачущий мяч')
+window = gr.GraphWin("Model", SIZE_X, SIZE_Y)
+
+coords = gr.Point(200, 200)
+velocity = gr.Point(5, 2)
+acceleration = gr.Point(0, 0.1)
+
+def wall(coords, velocity):
+    """Функция, проверяющая контакт мяча со стеной"""
+    if coords.x < 0 or coords.x > SIZE_X:
+        velocity.x = -velocity.x
+    if coords.y < 0 or coords.y > SIZE_Y:
+        velocity.y = -velocity.y
+
+def update_coords(coords, velocity):
+    return add(coords, velocity)
+
+def update_velocity(velocity, acceleration):
+    return add(velocity, acceleration)
+
+for t in range(300):
+    clear_window()
+    draw_ball(coords)
+    coords = update_coords(coords, velocity)
+    velocity = update_velocity(velocity, acceleration)
+    wall(coords, velocity)
+    gr.time.sleep(0.02)
+    t-=1
+
+window.getMouse()
+window.close()
+
