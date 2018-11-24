eba45624309728246a213371624f88d250b50c36
ArtemYeremeev <nice.art228@mail.ru>
2018-11-24 17:42:55 +0300

    Update Contest13

--- Labs/Contest№13.py
+++ Labs/Contest№13.py
@@ -1,1 +1,18 @@
 """MIPT Python Course. Contest13"""
+print('Exercise 1.')
+
+
+def z_func(s, n):
+    n = len(s)
+    z = [0] * n; left = right = 0
+    for i in range(1, n):
+        x = min(z[i - left], right - i + 1) if i <= right else 0
+        while 1 + x < n and s[x] == s[i + x]:
+            x += 1
+        z[i] = x
+        if i + x - 1 > right:
+            left, right = i, i + x - 1
+    return z
+
+
+z_func('abcab', 2)
