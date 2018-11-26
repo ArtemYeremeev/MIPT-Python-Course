981a741e0e5d8e38e2738e2d9674a37281373990
ArtemYeremeev <nice.art228@mail.ru>
2018-11-26 21:44:42 +0300

    Update Contest№13.py

--- Labs/Contest№13.py
+++ Labs/Contest№13.py
@@ -2,17 +2,41 @@
 print('Exercise 1.')
 
 
-def z_func(s, n):
-    n = len(s)
-    z = [0] * n; left = right = 0
-    for i in range(1, n):
-        x = min(z[i - left], right - i + 1) if i <= right else 0
-        while 1 + x < n and s[x] == s[i + x]:
-            x += 1
-        z[i] = x
-        if i + x - 1 > right:
-            left, right = i, i + x - 1
-    return z
+def z_func(s):
+    output = []
+    if not s: return output
+    i, slen = 1, len(s)
+    output.append(slen)
+    while i < slen:
+        left, right = 0, i
+        while right < slen and s[left] == s[right]:
+            left += 1
+            right += 1
+        output.append(left)
+        i += 1
+    return output
 
 
-z_func('abcab', 2)
+print(z_func('dasababadasdas'))
+
+print('Exercise 2.')
+
+
+def z_func_advanced(s1, s2):
+    output = []
+    if not s2 in s1: return output
+    i, slen = 1, len(s1)
+    output.append(slen)
+    while i < slen:
+        left, right = 0, i
+        while right < slen and s2[left] == s2[right]:
+            left += 1
+            right += 1
+        output.append(left)
+        i += 1
+    return output
+
+
+s1 = input('Type string -')
+s2 = input('Type substring -')
+print(z_func_advanced(s1, s2))
