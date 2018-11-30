58340f8704343b3a25c5c6f993a6f05ab17dc01f
ArtemYeremeev <nice.art228@mail.ru>
2018-11-30 19:55:47 +0300

    Contest 13 Updated

--- Labs/Contest№13.py
+++ Labs/Contest№13.py
@@ -40,3 +40,22 @@ def z_func_advanced(s1, s2):
 s1 = input('Type string -')
 s2 = input('Type substring -')
 print(z_func_advanced(s1, s2))
+
+print('Exercise 3.')
+
+
+def prefix_func(s, n):
+    pi = [0] * n
+    for i in range(n - 1):
+        for k in range(1, i + 1):
+            equal = True
+            for j in range(k):
+                if s[j] != s[i - k  + 1 + j]:
+                    equal = False
+                    break
+            if equal:
+                pi[i] = k
+    return pi
+
+
+print(prefix_func(2, 1))
