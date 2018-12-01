8e816446a67cae5e16487c40157221804d1c82e6
ArtemYeremeev <nice.art228@mail.ru>
2018-12-01 12:52:10 +0300

    Update Contest№13.py

--- Labs/Contest№13.py
+++ Labs/Contest№13.py
@@ -50,7 +50,7 @@ def prefix_func(s, n):
         for k in range(1, i + 1):
             equal = True
             for j in range(k):
-                if s[j] != s[i - k  + 1 + j]:
+                if s[j] != s[i - k + 1 + j]:
                     equal = False
                     break
             if equal:
@@ -59,3 +59,28 @@ def prefix_func(s, n):
 
 
 print(prefix_func(2, 1))
+
+print('Exercise 4')
+
+x = input('Введите строку - ')
+s = input('Введите подстроку - ')
+
+
+def kmp_search_by_compil(s, x):
+    """Осуществляет поиск подстроки
+    в строке методом КМП"""
+    d = {0: 0}
+    template = x + '#' + s
+    for i in range(1, len(template)):
+        j = d[i-1]
+        while j > 0 and template[j] != template[i]:
+            j = d[j-1]
+        if template[j] == template[i]:
+            j += 1
+        d[i] = j
+        if j == len(x):
+            return i
+    return d
+
+
+print(kmp_search_by_compil(s, x))
