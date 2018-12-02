cce6a3e684dbb70cfbcabae88e6663f43e06ea31
ArtemYeremeev <nice.art228@mail.ru>
2018-12-02 13:32:47 +0300

    Create Contest№14.py

---
+++ Labs/Contest№14.py
@@ -0,0 +1,61 @@
+"""MIPT Python Course. Contest14"""
+from random import randint
+from lib import _stack
+
+print('Exercise 1. Stack')
+
+print('Стек пуст?', _stack.is_empty())
+
+for x in range(5):
+    _stack.push(randint(1, 10))
+print('В стеке находятся следующие числа', _stack.output())
+for x in range(5):
+    print('Достаю верхний элемент стека - ', _stack.pop())
+
+print('Стек пуст?', _stack.is_empty())
+
+
+print('Exercise 2. Bracers structures ')
+
+bracers = input('Введите скобочную последовательность - ')
+
+
+def check_bracers_sequence(bracers):
+    """Проверяет корректность скобочной последовательности"""
+    for brace in bracers:
+        if brace not in "{}[]":
+            continue
+        if brace in '{[':
+            _stack.push(brace)
+        else:
+            if _stack.is_empty():
+                return False
+            left = _stack.pop()
+            if left == '{':
+                right = '}'
+            elif left == '[':
+                right = ']'
+            if right != brace:
+                return False
+    return _stack.is_empty()
+
+
+check_bracers_sequence(bracers)
+
+print('Exercise 3. Reversive Polish notation')
+
+ints = str(list(range(0, 11)))
+bracers = ['(', ')']
+operands = ['+', '-', '*', '/']
+x = list('(3+4*(2-1))/5')
+print(x)
+for i in range(len(x)):
+    if x[i] in bracers:
+        pass
+    if x[i] in ints:
+        _stack.push(x[i])
+    if x[i] in operands:
+        _stack.push(x[i])
+print(_stack.output())
+
+
